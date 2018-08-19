c433c070841ec941882fc4742b1710b035c10928
atadkase <ashutosh.tadkase@gmail.com>
2018-08-19 23:08:24 +0000

    Working with swerves

---
+++ model.py
@@ -0,0 +1,50 @@
+import csv
+import cv2
+import numpy as np
+
+lines = []
+with open('./test_data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+images = []
+measurements = []
+# print(lines[1])
+# print(lines[100])
+for line in lines:
+    current_path = './test_data/' + line[0]
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+    flipped_image = np.fliplr(image)
+    flipped_measurement = -measurement
+    images.append(flipped_image)
+    measurements.append(flipped_measurement)
+    
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+from keras.models import Sequential, Model
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D
+
+model = Sequential()
+model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5))
+model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
+model.add(Convolution2D(64,3,3, activation='relu'))
+model.add(Convolution2D(64,3,3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+
+model.save('model.h5')
+
+        
